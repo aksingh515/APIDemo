ac67d4fd821cc957129e3afbcc31c3141b2c29d8
Ashutosh <akkiashusms@gmail.com>
2023-01-01 01:32:53 +0530

    First Commit by Asian TimeZone

---
+++ src/com/testNg/Test/TestngTest.java
@@ -0,0 +1,27 @@
+package com.testNg.Test;
+
+import org.testng.annotations.Test;
+
+public class TestngTest {
+	@Test
+	public void demo() {
+		System.out.println("Hello TestNg");
+		System.out.println("Hello Java");
+		System.out.println("Hello Selenium");
+
+	}
+
+	@Test
+	public void display() {
+		System.out.println("Web Automation");
+		System.out.println("RestAPI Automation");
+		System.out.println("Hello TestNg");
+		System.out.println("Hello Java");
+		System.out.println("Hello Selenium");
+	}
+
+	@Test
+	public void demo2() {
+		System.out.println("Bye");
+	}
+}
---
+++ src/com/testNg/Test/TestngTest2.java
@@ -0,0 +1,29 @@
+package com.testNg.Test;
+
+import org.testng.annotations.Test;
+
+public class TestngTest2 
+{
+	@Test
+	public void ploan()
+	{
+		System.out.println("Personal Loan");
+		System.out.println("Car Loan");
+		System.out.println("House Loan");
+	}
+	
+	public void withdraw()
+	{
+		System.out.println("Salary");
+		System.out.println("Expences");
+		System.out.println("Bills");
+	}
+	
+	public void insurance()
+	{
+		System.out.println("Self Insurance.");
+		System.out.println("Child Isurance");
+		System.out.println("Family Isurance");
+		System.out.println("Car Issurance");
+	}
+}
